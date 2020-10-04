de0708c95a85aba41ffaf106542b5018a5b2ae3b
luzhoujin <Jy123456>
2020-10-04 21:26:50 +0800

    netty 长连接网页版聊天室

---
+++ src/main/java/com/JolyouLu/netty/websocket/MyTextWebSocketFrameHandler.java
@@ -0,0 +1,75 @@
+package com.JolyouLu.netty.websocket;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * @Author: LZJ
+ * @Date: 2020/10/4 13:21
+ * @Version 1.0
+ * TextWebSocketFrame 表示一个文本帧(frame)
+ */
+public class MyTextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+    private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
+        System.out.println("服务器端收到消息:"+msg.text());
+        Channel channel = ctx.channel();
+        //遍历channelGroup根据不同情况，回送不同消息
+        channelGroup.forEach(ch -> {
+            if (channel != ch){ //不是读取的channel，转发
+                ch.writeAndFlush(new TextWebSocketFrame("[用户]"+channel.remoteAddress() +" 发送了消息: "+msg.text()));
+            }else {
+                ch.writeAndFlush(new TextWebSocketFrame("[自己]发送了消息: "+msg.text()));
+            }
+        });
+    }
+
+    //当客户端连接后，会触发
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        Channel channel = ctx.channel();
+        //将该客户加入聊天的信息推送给其它在线的客户端
+        //该方法会把channelGroup的所有channel遍历，调用writeAndFlush方法
+        channelGroup.writeAndFlush(new TextWebSocketFrame("[客户端]"+channel.remoteAddress()+"加入聊天"));
+        channelGroup.add(channel);
+    }
+
+    //当客户端断开连接后，会触发
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        //提示其它所有用户当前用户已经离线
+        Channel channel = ctx.channel();
+        channelGroup.writeAndFlush(new TextWebSocketFrame("[客户端]"+channel.remoteAddress()+"离开了"));
+        System.out.println("channelGroup size"+channelGroup.size());
+    }
+
+    //channel 处于活动状态执行该方法
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().id().asLongText()+"上线了~");
+    }
+
+    //channel 处于非活动状态执行该方法
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(ctx.channel().id().asLongText()+"离线了~");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("异常发生"+cause.getMessage());
+        ctx.close(); //关闭连接
+    }
+}
